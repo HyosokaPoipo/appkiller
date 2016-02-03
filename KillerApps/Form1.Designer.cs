874b69cf700db83446f6a08563d08a509eea9f52
Maryadi <maryadi@elephanttalk.com>
2016-02-03 14:44:16 +0700

    Finishing.....

--- KillerApps/Form1.Designer.cs
+++ KillerApps/Form1.Designer.cs
@@ -51,6 +51,7 @@
             this.killerButton.TabIndex = 1;
             this.killerButton.Text = "Kill Process";
             this.killerButton.UseVisualStyleBackColor = true;
+            this.killerButton.Click += new System.EventHandler(this.killerButton_Click);
             // 
             // Form1
             // 
@@ -61,6 +62,7 @@
             this.Controls.Add(this.listBoxApps);
             this.Name = "Form1";
             this.Text = "AppKiller";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- KillerApps/Form1.cs
+++ KillerApps/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,30 @@ namespace KillerApps
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void killerButton_Click(object sender, EventArgs e)
+        {
+            string selectedApp = listBoxApps.Text;
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (selectedApp.Contains(p.ProcessName))
+                {
+                    p.Kill();
+                    listBoxApps.Items.Remove(selectedApp);
+                    MessageBox.Show(p.ProcessName + "Telah dihentikan...!");
+                    break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Nampilin daftar aplikasi running di listBox
+            foreach (Process p in Process.GetProcesses())
+            {
+                listBoxApps.Items.Add(p.ProcessName+"===>"+p.MainWindowTitle);
+            }
         }
     }
 }
