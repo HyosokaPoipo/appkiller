74b52eb65919eda133bbee5a935e2b240ed953fd
Maryadi <maryadi@elephanttalk.com>
2016-02-03 14:08:02 +0700

    Adjusting GUI..

--- KillerApps/Form1.Designer.cs
+++ KillerApps/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxApps = new System.Windows.Forms.ListBox();
+            this.killerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxApps
+            // 
+            this.listBoxApps.FormattingEnabled = true;
+            this.listBoxApps.HorizontalScrollbar = true;
+            this.listBoxApps.Location = new System.Drawing.Point(23, 34);
+            this.listBoxApps.Name = "listBoxApps";
+            this.listBoxApps.ScrollAlwaysVisible = true;
+            this.listBoxApps.Size = new System.Drawing.Size(373, 342);
+            this.listBoxApps.Sorted = true;
+            this.listBoxApps.TabIndex = 0;
+            // 
+            // killerButton
+            // 
+            this.killerButton.Location = new System.Drawing.Point(472, 214);
+            this.killerButton.Name = "killerButton";
+            this.killerButton.Size = new System.Drawing.Size(75, 23);
+            this.killerButton.TabIndex = 1;
+            this.killerButton.Text = "button1";
+            this.killerButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 403);
+            this.Controls.Add(this.killerButton);
+            this.Controls.Add(this.listBoxApps);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxApps;
+        private System.Windows.Forms.Button killerButton;
     }
 }
 
